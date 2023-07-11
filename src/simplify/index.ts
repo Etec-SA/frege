b830d823c0a8ac609859a6219f5bf7598020b75d
iamthepoe <luca1901p@gmail.com>
2023-07-11 19:54:06 -0300

    simplify ->

--- src/simplify/index.ts
+++ src/simplify/index.ts
@@ -1,3 +1,17 @@
 export class Simplify {
   private static implicationRegex = /(.*)->(.*)/;
+
+  private static simplifyImplication(expression: string): string {
+    const matches = expression.match(this.implicationRegex);
+
+    if (!matches) return expression;
+
+    if (matches.length != 3) return expression;
+
+    const p = matches[1].trim();
+    const q = matches[2].trim();
+    const negatedP = `¬${p}`;
+
+    return `(${negatedP} ∨ ${q})`;
+  }
 }
