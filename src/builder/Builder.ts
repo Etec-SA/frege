e903874e7e067dab17f10ad6933459a5210e246e
iamthepoe <luca1901p@gmail.com>
2023-07-19 13:29:28 -0300

    first version of builder

--- src/builder/Builder.ts
+++ src/builder/Builder.ts
@@ -1,11 +1,43 @@
 import { Formula } from "./interfaces/formula";
 
-export class Builder {
-  static disjunction(x: Formula, y: Formula) {
-    return `${x.toUpperCase()} ∨ ${y.toUpperCase()}`;
+export class builder {
+
+  private static operations = {
+    'Biconditional': this.biconditional,
+    'Conjunction': this.conjunction,
+    'Disjunction': this.disjunction,
+    'Implication': this.implication
+  }
+  
+  private static biconditional(left: Formula, right: Formula){
+    return `(${left} ↔ ${right})`;
   }
 
-  static conjunction(x: Formula, y: Formula) {
-    return `${x.toUpperCase()} ∧ ${y.toUpperCase()}`;
+  private static conjunction(left: Formula, right: Formula) {
+    return `(${left} ∧ ${right})`;
+  }
+
+  private static disjunction(left: Formula, right: Formula) {
+    return `(${left} ∨ ${right})`;
+  }
+
+  private static implication(left: Formula, right: Formula){
+    return `(${left} -> ${right})`;
+  }
+
+  public static buildFormula(formula: Formula){
+    if(typeof formula === 'string') return formula;
+
+    if('operation' in formula && formula.operation === 'Negation')
+      return `¬(${this.buildFormula(formula.value)})`;
+
+    if(!('operation' in formula))
+      throw new Error('Fórmula inválida'); 
+    
+    const left =  this.buildFormula(formula.left);
+    const right = this.buildFormula(formula.right);
+    const operation = formula.operation;
+
+    return this.operations[operation](left, right);
   }
 }
