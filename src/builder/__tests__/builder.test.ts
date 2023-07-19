847cd41203742f3902db76695abeea0c205b16fb
iamthepoe <luca1901p@gmail.com>
2023-07-19 16:17:54 -0300

    remove old tests

--- src/builder/__tests__/builder.test.ts
+++ src/builder/__tests__/builder.test.ts
@@ -1,30 +1,9 @@
 import assert from "assert/strict";
 import { describe, it } from "node:test";
-import { Builder } from "../Builder";
+import { builder } from "../Builder";
 
 describe('Builder', () => {
   it('should be defined', () => {
-    assert.ok(Builder);
+    assert.ok(builder);
   });
-
-  it('should build a disjunction', () => {
-    const disjunction = Builder.disjunction;
-    assert.equal(disjunction('p', 'q'), 'P ∨ Q');
-    assert.equal(disjunction('(p ∨ q)', 'q'), '(P ∨ Q) ∨ Q');
-    assert.equal(
-      disjunction('((p -> q) -> (q -> (p -> q)))', '(q <-> p)'),
-      '((P -> Q) -> (Q -> (P -> Q))) ∨ (Q <-> P)'
-    );
-  });
-
-  it('should build a conjunction', () => {
-    const conjunction = Builder.conjunction;
-    assert.equal(conjunction('p', 'q'), 'P ∧ Q');
-    assert.equal(conjunction('(p ∨ q)', 'q'), '(P ∨ Q) ∧ Q');
-    assert.equal(
-      conjunction('((p -> q) -> (q -> (p -> q)))', '(q <-> p)'),
-      '((P -> Q) -> (Q -> (P -> Q))) ∧ (Q <-> P)'
-    );
-  });
-
 })
