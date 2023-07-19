243911adf84736791aef2b65623aee22cfeb02de
iamthepoe <luca1901p@gmail.com>
2023-07-19 00:58:47 -0300

    conjunction test

--- src/builder/__tests__/builder.test.ts
+++ src/builder/__tests__/builder.test.ts
@@ -7,7 +7,7 @@ describe('Builder', () => {
     assert.ok(Builder);
   });
 
-  it('should build an disjunction', () => {
+  it('should build a disjunction', () => {
     const disjunction = Builder.disjunction;
     assert.equal(disjunction('p', 'q'), 'P ∨ Q');
     assert.equal(disjunction('(p ∨ q)', 'q'), '(P ∨ Q) ∨ Q');
@@ -16,4 +16,15 @@ describe('Builder', () => {
       '((P -> Q) -> (Q -> (P -> Q))) ∨ (Q <-> P)'
     );
   });
+
+  it('should build a conjunction', () => {
+    const conjunction = Builder.conjunction;
+    assert.equal(conjunction('p', 'q'), 'P ∧ Q');
+    assert.equal(conjunction('(p ∨ q)', 'q'), '(P ∨ Q) ∧ Q');
+    assert.equal(
+      conjunction('((p -> q) -> (q -> (p -> q)))', '(q <-> p)'),
+      '((P -> Q) -> (Q -> (P -> Q))) ∧ (Q <-> P)'
+    );
+  });
+
 })
