043ac0fdda9f0a20f54ab44f858792be85fef66b
iamthepoe <luca1901p@gmail.com>
2023-07-19 00:56:21 -0300

    conjunction method

--- src/builder/Builder.ts
+++ src/builder/Builder.ts
@@ -1,5 +1,11 @@
-export class Builder{
-  static disjunction(x: string, y: string){
+import { Formula } from "./interfaces/formula";
+
+export class Builder {
+  static disjunction(x: Formula, y: Formula) {
     return `${x.toUpperCase()} ∨ ${y.toUpperCase()}`;
+  }
+
+  static conjunction(x: Formula, y: Formula) {
+    return `${x.toUpperCase()} ∧ ${y.toUpperCase()}`;
   }
 }
