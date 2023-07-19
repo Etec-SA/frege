78a65c948645da195823019d7c862571c6e8ad49
iamthepoe <luca1901p@gmail.com>
2023-07-19 18:50:26 -0300

    simplify biconditional

--- src/simplify/__tests__/simplify.test.ts
+++ src/simplify/__tests__/simplify.test.ts
@@ -3,9 +3,10 @@ import { describe, it } from "node:test";
 import { Implication } from "src/builder/interfaces/operations/implication";
 import { simplify } from "../Simplify";
 import { builder } from "src/builder/Builder";
+import { Biconditional } from "src/builder/interfaces/operations/biconditional";
 
 describe('Simplify', () => {
-  it('should simplify (P -> Q) to (¬P v Q)', () => {
+  it('should simplify (P -> Q) to (¬(P) v Q)', () => {
     const implication: Implication = {
       operation: 'Implication',
       left: 'P',
@@ -17,4 +18,18 @@ describe('Simplify', () => {
     assert.equal(result.operation, 'Disjunction');
     assert.equal(builder.buildFormula(result), '(¬(P) ∨ Q)')
   });
+
+  it('should simplify (P <-> Q) to (P -> Q) ^ (Q -> P)', () => {
+    const biconditional: Biconditional = {
+      operation: 'Biconditional',
+      left: 'P',
+      right: 'Q'
+    }
+
+    const result = simplify.biconditional(biconditional);
+    assert.ok(result);
+    assert.equal(result.operation, 'Conjunction');
+    assert.equal(builder.buildFormula(result), '((P -> Q) ∧ (Q -> P))')
+  });
+
 });
