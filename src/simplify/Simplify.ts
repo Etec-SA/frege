0e04af3b7c527bd0f5ab85910bdac4b0712739c7
iamthepoe <luca1901p@gmail.com>
2023-07-19 19:38:09 -0300

    simplifyFormula

--- src/simplify/Simplify.ts
+++ src/simplify/Simplify.ts
@@ -1,9 +1,30 @@
+import { builder } from "src/builder/Builder";
+import { Formula } from "src/builder/interfaces/formula";
 import { Biconditional } from "src/builder/interfaces/operations/biconditional";
 import { Conjunction } from "src/builder/interfaces/operations/conjunction";
 import { Disjunction } from "src/builder/interfaces/operations/disjunction";
 import { Implication } from "src/builder/interfaces/operations/implication";
 
 export class simplify {
+
+  public static simplifyFormula(x: Formula): Formula {
+    if (typeof x === 'string') return x;
+
+    if (!('operation' in x)) return x;
+
+    if (x.operation === 'Biconditional')
+      return this.simplifyFormula(this.biconditional(x));
+
+    if (x.operation === 'Implication')
+      return this.implication(x);
+
+    if (x.operation === 'Conjunction')
+      return this.conjunction(x);
+
+    if (x.operation === 'Disjunction')
+      return this.disjunction(x);
+  }
+
   public static biconditional(x: Biconditional) {
     const firstImplication: Implication = {
       operation: 'Implication',
@@ -38,5 +59,24 @@ export class simplify {
 
     return simplifiedImplication;
   }
-}
 
+  public static conjunction(x: Conjunction): Formula {
+    const simplifiedConjunction: Conjunction = {
+      operation: 'Conjunction',
+      left: this.simplifyFormula(x.left),
+      right: this.simplifyFormula(x.right),
+    };
+
+    return simplifiedConjunction;
+  }
+
+  public static disjunction(x: Disjunction): Formula {
+    const simplifiedDisjunction: Disjunction = {
+      operation: 'Disjunction',
+      left: this.simplifyFormula(x.left),
+      right: this.simplifyFormula(x.right),
+    };
+
+    return simplifiedDisjunction;
+  }
+}
