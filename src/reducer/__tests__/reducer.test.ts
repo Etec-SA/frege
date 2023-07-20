38453d7521d6037113fbba37bbe1660b6cfdf41c
iamthepoe <luca1901p@gmail.com>
2023-07-20 01:52:04 -0300

    reduce complex formula

--- src/reducer/__tests__/reducer.test.ts
+++ src/reducer/__tests__/reducer.test.ts
@@ -1,59 +1,42 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
 import { Implication } from "src/builder/interfaces/operations/implication";
-import { simplify } from "../Simplify";
+import { reducer } from "../Reducer";
 import { builder } from "src/builder/Builder";
 import { Biconditional } from "src/builder/interfaces/operations/biconditional";
 import { Disjunction } from "src/builder/interfaces/operations/disjunction";
 import { Conjunction } from "src/builder/interfaces/operations/conjunction";
 
-describe('Simplify', () => {
-  it('should simplify (P -> Q) to (¬(P) v Q)', () => {
+describe('Reducer', () => {
+  it('should reduce (P -> Q) to (¬(P) v Q)', () => {
     const implication: Implication = {
       operation: 'Implication',
       left: 'P',
       right: 'Q'
     }
 
-    const result = simplify.implication(implication);
+    const result = reducer.reduceFormula(implication);
     assert.ok(result);
     assert.equal(result.operation, 'Disjunction');
     assert.equal(builder.buildFormula(result), '(¬(P) ∨ Q)')
   });
 
-  it('should simplify (P <-> Q) to (P -> Q) ^ (Q -> P)', () => {
+  it('should reduce (P <-> Q) to ((¬(P) v Q) ^ (¬(Q) v P))', () => {
     const biconditional: Biconditional = {
       operation: 'Biconditional',
       left: 'P',
       right: 'Q'
     }
 
-    const result = simplify.biconditional(biconditional);
-    assert.ok(result);
-    assert.equal(result.operation, 'Conjunction');
-    assert.equal(builder.buildFormula(result), '((P -> Q) ∧ (Q -> P))')
-  });
-
-  it('should simplify (P<->Q) to ((¬(P) v Q) ^ (¬(Q) v P))', () => {
-    const biconditional: Biconditional = {
-      operation: 'Biconditional',
-      left: 'P',
-      right: 'Q'
-    }
-
-    const result = simplify.simplifyFormula(biconditional);
+    const result = reducer.reduceFormula(biconditional);
 
     assert.ok(result);
     assert.equal(result.operation, 'Conjunction');
     assert.equal(builder.buildFormula(result), '((¬(P) ∨ Q) ∧ (¬(Q) ∨ P))')
   });
 
-  it('should simplify (P->(Q->(P->Q)) <-> (P v Q)) to ', () => {
-    // (P->(Q->(P->Q)) <-> (P v Q)) 
-    // ((P->(Q->(P->Q)))) -> (P v Q))) ^ ((P v Q) -> (P->(Q->(P->Q))))
-    // P->(Q->(P->Q))
-    // ¬(P) v (Q->(P->Q))
-    // (¬(P) v (¬(Q) v (¬(P) v Q)) -> (P v Q))
+  it(`should reduce "(P -> (Q -> (P -> Q)) <-> (P v Q))" to 
+    "((¬((¬(P) ∨ (¬(Q) ∨ (¬(P) ∨ Q)))) ∨ (P ∨ Q)) ∧ (¬((P ∨ Q)) ∨ (¬(P) ∨ (¬(Q) ∨ (¬(P) ∨ Q)))))"`, () => {
 
     const firstImplication: Implication = {
       operation: 'Implication',
@@ -85,8 +68,9 @@ describe('Simplify', () => {
       right: disjunction
     }
 
-    const result = simplify.toCNF(conjunction);
-    console.log(builder.buildFormula(result));
-
-  })
+    const result = reducer.reduceFormula(conjunction);
+    assert.ok(result);
+    assert.equal(result.operation, 'Conjunction')
+    assert.equal(builder.buildFormula(result), '((¬((¬(P) ∨ (¬(Q) ∨ (¬(P) ∨ Q)))) ∨ (P ∨ Q)) ∧ (¬((P ∨ Q)) ∨ (¬(P) ∨ (¬(Q) ∨ (¬(P) ∨ Q)))))');
+  });
 });
