2ead81bc5204b9775477036b96128ad908306454
iamthepoe <luca1901p@gmail.com>
2023-07-20 01:53:20 -0300

    add reducer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { builder } from "./builder/Builder";
+import { reducer } from "./reducer/Reducer";
 
 export class Frege {
   public static builder = builder;
+  public static reducer = reducer;
 }
