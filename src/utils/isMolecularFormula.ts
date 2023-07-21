bd90383d31bc1401f20d1d5c0bf55ac492b3438e
iamthepoe <luca1901p@gmail.com>
2023-07-21 00:16:06 -0300

    change function name

--- src/utils/isMolecularFormula.ts
+++ src/utils/isMolecularFormula.ts
@@ -1,7 +1,7 @@
 import { Formula } from "src/builder/interfaces/formula";
 import { MolecularFormula } from "src/builder/interfaces/molecular-formula";
 
-export function isCompositeFormula(formula: Formula): formula is MolecularFormula {
+export function isMolecularFormula(formula: Formula): formula is MolecularFormula {
     if(typeof formula === 'string') return false;
 
     if(formula.operation === 'Negation') return false;
