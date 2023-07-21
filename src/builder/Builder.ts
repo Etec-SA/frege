d6a7927ef506e568b86904b1477d7fb8d9755280
iamthepoe <luca1901p@gmail.com>
2023-07-21 12:37:54 -0300

    jsdoc and explicit type

--- src/builder/Builder.ts
+++ src/builder/Builder.ts
@@ -1,4 +1,11 @@
+import { InvalidFormulaException } from 'src/exceptions/invalid-formula.exception';
 import { Formula } from './interfaces/formula';
+
+/**
+ * A class that provides methods to build formulas with
+ * the artificial language of logic syntax.
+ */
+
 
 export class builder {
   private static operations = {
@@ -24,18 +31,30 @@ export class builder {
     return `(${left} -> ${right})`;
   }
 
-  public static buildFormula(formula: Formula) {
+  private static buildRecursively(formula: Formula) {
     if (typeof formula === 'string') return formula;
 
     if ('operation' in formula && formula.operation === 'Negation')
       return `¬(${this.buildFormula(formula.value)})`;
 
-    if (!('operation' in formula)) throw new Error('Fórmula inválida');
+    if (!('operation' in formula)) throw new InvalidFormulaException('Invalid Formula.');
 
     const left = this.buildFormula(formula.left);
     const right = this.buildFormula(formula.right);
     const operation = formula.operation;
 
     return this.operations[operation](left, right);
+  }
+
+  /**
+   * Builds a formula with the syntax of logic.
+   * @param formula - The logical formula to build.
+   * @returns The builded logical formula. 
+   * @throws {InvalidFormulaException}
+   */
+
+  public static buildFormula(formula: Formula) {
+    const result = this.buildRecursively(formula);
+    return result as string;
   }
 }
