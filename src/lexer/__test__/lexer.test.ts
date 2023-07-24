6aeda5337a0e007f9b3ce221bc6ee2746afd3ea2
iamthepoe <luca1901p@gmail.com>
2023-07-23 21:27:08 -0300

    negation test

--- src/lexer/__test__/lexer.test.ts
+++ src/lexer/__test__/lexer.test.ts
@@ -1,6 +1,8 @@
 import assert from "assert";
 import { describe, it, beforeEach } from "node:test";
 import { Lexer } from "../Lexer";
+import { TokenType } from "src/types/tokens/tokens";
+import { Token } from "src/types/tokens/tokens";
 
 let lexer: Lexer;
 
@@ -11,5 +13,17 @@ describe('Lexer', ()=>{
 
     it('should be defined', ()=>{
         assert.ok(lexer);
+    });
+
+    it('should lex ¬P', ()=>{
+        const expected: Token[] = [
+            { type: 'operator', value: '¬' },
+            { type: 'variable', value: 'P' }
+        ];
+
+        lexer.input = '¬P';
+        let result = lexer.lex();
+        assert.equal(result.length, 2);
+        assert.deepEqual(result, expected);
     });
 })
