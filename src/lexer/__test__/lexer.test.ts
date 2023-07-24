0fe98bf167a240f9abdfdd3998b7f29cafe592bc
iamthepoe <luca1901p@gmail.com>
2023-07-23 21:33:22 -0300

    boundary test

--- src/lexer/__test__/lexer.test.ts
+++ src/lexer/__test__/lexer.test.ts
@@ -1,29 +1,42 @@
-import assert from "assert";
-import { describe, it, beforeEach } from "node:test";
-import { Lexer } from "../Lexer";
-import { TokenType } from "src/types/tokens/tokens";
-import { Token } from "src/types/tokens/tokens";
+import assert from 'assert';
+import { describe, it, beforeEach } from 'node:test';
+import { Lexer } from '../Lexer';
+import { TokenType } from 'src/types/tokens/tokens';
+import { Token } from 'src/types/tokens/tokens';
 
 let lexer: Lexer;
 
-describe('Lexer', ()=>{
-    beforeEach(()=>{
-        lexer = new Lexer('');
-    });
+describe('Lexer', () => {
+  beforeEach(() => {
+    lexer = new Lexer('');
+  });
 
-    it('should be defined', ()=>{
-        assert.ok(lexer);
-    });
+  it('should be defined', () => {
+    assert.ok(lexer);
+  });
 
-    it('should lex ¬P', ()=>{
-        const expected: Token[] = [
-            { type: 'operator', value: '¬' },
-            { type: 'variable', value: 'P' }
-        ];
+  it('should lex ¬P', () => {
+    const expected: Token[] = [
+      { type: 'operator', value: '¬' },
+      { type: 'variable', value: 'P' },
+    ];
 
-        lexer.input = '¬P';
-        let result = lexer.lex();
-        assert.equal(result.length, 2);
-        assert.deepEqual(result, expected);
-    });
-})
+    lexer.input = '¬P';
+    const result = lexer.lex();
+    assert.equal(result.length, 2);
+    assert.deepEqual(result, expected);
+  });
+
+  it('should lex (P)', () => {
+    const expected: Token[] = [
+      { type: 'boundary', value: '(' },
+      { type: 'variable', value: 'P' },
+      { type: 'boundary', value: ')' },
+    ];
+
+    lexer.input = '(P)';
+    const result = lexer.lex();
+    assert.equal(result.length, 3);
+    assert.deepEqual(result, expected);
+  });
+});
