42fd98d8d7a0d165a5749f85cf1e85f2dcd14039
iamthepoe <luca1901p@gmail.com>
2023-07-31 22:26:19 -0300

    style: fix the style of file

--- src/calculator/Calculator.ts
+++ src/calculator/Calculator.ts
@@ -1,27 +1,30 @@
-import frege from "src/index";
-import { Formula } from "src/types/formulas/formula";
-import { Biconditional, BinaryOperation, Conjunction, Disjunction, Implication } from "src/types/operations/binary-operations";
-import { PropositionalVariable } from "src/types/operations/propositional-variable";
-import { Negation } from "src/types/operations/unary-operation";
+import frege from 'src/index';
+import { Formula } from 'src/types/formulas/formula';
+import {
+  Biconditional,
+  BinaryOperation,
+  Conjunction,
+  Disjunction,
+  Implication,
+} from 'src/types/operations/binary-operations';
+import { PropositionalVariable } from 'src/types/operations/propositional-variable';
+import { Negation } from 'src/types/operations/unary-operation';
 
-type PropositionalVariables<T extends Formula> =
-  T extends PropositionalVariable ? T :
-  T extends Negation ? PropositionalVariables<T["value"]> :
-  T extends BinaryOperation ? PropositionalVariables<T["left"]> | PropositionalVariables<T["right"]> :
-  never;
-
-type PropositionalVariableValues<T extends Formula> = {
-  [K in PropositionalVariables<T>]: boolean;
+type PropositionalVariableValues = {
+  [K in PropositionalVariable]?: boolean;
 };
 
 export class Calculator {
-  public static evaluate(formula: Formula | string, values: PropositionalVariableValues<Formula>): boolean {
+  public static evaluate<T extends Formula>(
+    formula: T | string,
+    values: PropositionalVariableValues
+  ): boolean {
     if (typeof formula === 'string' && formula.length > 1) {
       const parsedFormula = frege.parse.toFormulaObject(formula);
-      return this.evaluate(parsedFormula, values);
+      return this.evaluate(parsedFormula as T, values);
     }
 
-    if (typeof formula === 'string') return values[formula];
+    if (typeof formula === 'string') return values[`${formula}`];
 
     if (formula.operation === 'Implication') {
       return this.evaluateImplication(formula, values);
@@ -46,31 +49,46 @@ export class Calculator {
     throw new Error('Invalid formula operation');
   }
 
-  private static evaluateImplication(formula: Implication, values: PropositionalVariableValues<Formula>): boolean {
+  private static evaluateImplication(
+    formula: Implication,
+    values: PropositionalVariableValues
+  ): boolean {
     const left = this.evaluate(formula.left, values);
     const right = this.evaluate(formula.right, values);
     return !left || right;
   }
 
-  private static evaluateBiconditional(formula: Biconditional, values: PropositionalVariableValues<Formula>): boolean {
+  private static evaluateBiconditional(
+    formula: Biconditional,
+    values: PropositionalVariableValues
+  ): boolean {
     const left = this.evaluate(formula.left, values);
     const right = this.evaluate(formula.right, values);
     return (left && right) || (!left && !right);
   }
 
-  private static evaluateConjunction(formula: Conjunction, values: PropositionalVariableValues<Formula>): boolean {
+  private static evaluateConjunction(
+    formula: Conjunction,
+    values: PropositionalVariableValues
+  ): boolean {
     const left = this.evaluate(formula.left, values);
     const right = this.evaluate(formula.right, values);
     return left && right;
   }
 
-  private static evaluateDisjunction(formula: Disjunction, values: PropositionalVariableValues<Formula>): boolean {
+  private static evaluateDisjunction(
+    formula: Disjunction,
+    values: PropositionalVariableValues
+  ): boolean {
     const left = this.evaluate(formula.left, values);
     const right = this.evaluate(formula.right, values);
     return left || right;
   }
 
-  private static evaluateNegation(formula: Negation, values: PropositionalVariableValues<Formula>): boolean {
+  private static evaluateNegation(
+    formula: Negation,
+    values: PropositionalVariableValues
+  ): boolean {
     const value = this.evaluate(formula.value, values);
     return !value;
   }
