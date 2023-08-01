80123bb3ea914d1c9a53b3e7a32e1398034bdd31
iamthepoe <luca1901p@gmail.com>
2023-08-01 20:04:36 -0300

    test(calculator): create tests for truth table generator

--- src/calculator/__tests__/calculator.test.ts
+++ src/calculator/__tests__/calculator.test.ts
@@ -96,20 +96,20 @@ describe('Calculator', () => {
       assert.equal(case2, true);
     });
 
-    it('should evaluate A->(B->A)', ()=>{
-        const operation = 'A->(B->A)';
-        const case1 = Calculator.evaluate(operation, { A: true, B: true });
-        const case2 = Calculator.evaluate(operation, { A: false, B: true });
-        const case3 = Calculator.evaluate(operation, { A: true, B: false });
-        const case4 = Calculator.evaluate(operation, { A: false, B: false });
+    it('should evaluate A->(B->A)', () => {
+      const operation = 'A->(B->A)';
+      const case1 = Calculator.evaluate(operation, { A: true, B: true });
+      const case2 = Calculator.evaluate(operation, { A: false, B: true });
+      const case3 = Calculator.evaluate(operation, { A: true, B: false });
+      const case4 = Calculator.evaluate(operation, { A: false, B: false });
 
-        assert.equal(case1, true);
-        assert.equal(case2, true);
-        assert.equal(case3, true);
-        assert.equal(case4, true);
+      assert.equal(case1, true);
+      assert.equal(case2, true);
+      assert.equal(case3, true);
+      assert.equal(case4, true);
     });
 
-    it('should evaluate ¬(¬(A ∧ ¬B) ∧ ¬(A ∧ ¬B))', ()=>{
+    it('should evaluate ¬(¬(A ∧ ¬B) ∧ ¬(A ∧ ¬B))', () => {
       const operation = '¬A ∨ (¬B ∨ A)';
       const case1 = Calculator.evaluate(operation, { A: true, B: true });
       const case2 = Calculator.evaluate(operation, { A: false, B: true });
@@ -120,9 +120,9 @@ describe('Calculator', () => {
       assert.equal(case2, true);
       assert.equal(case3, true);
       assert.equal(case4, true);
-    })
+    });
 
-    it('should evaluate ¬(¬ A ∧ ¬(B->A))', ()=>{
+    it('should evaluate ¬(¬ A ∧ ¬(B->A))', () => {
       const operation = '¬(A ∧ ¬¬(B ∧ ¬A))';
 
       const case1 = Calculator.evaluate(operation, { A: true, B: true });
@@ -134,6 +134,164 @@ describe('Calculator', () => {
       assert.equal(case2, true);
       assert.equal(case3, true);
       assert.equal(case4, true);
+    });
+  });
+
+  describe('generateTruthTable', () => {
+    it('should generate a truth table for ¬P', () => {
+      const output = Calculator.generateTruthTable('¬P');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['P', '¬P']);
+      assert.deepStrictEqual(truthCombinations, [[0], [1]]);
+      assert.deepStrictEqual(mainFormulaValues, [true, false]);
+    });
+
+    it('should generate a truth table for P -> Q', () => {
+      const output = Calculator.generateTruthTable('P -> Q');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['P', 'Q', 'P -> Q']);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [true, true, false, true]);
+    });
+
+    it('should generate a truth table for P <-> Q', () => {
+      const output = Calculator.generateTruthTable('P <-> Q');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['P', 'Q', 'P <-> Q']);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [true, false, false, true]);
+    });
+
+    it('should generate a truth table for P ∧ Q', () => {
+      const output = Calculator.generateTruthTable('P ∧ Q');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['P', 'Q', 'P ∧ Q']);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [false, false, false, true]);
+    });
+
+    it('should generate a truth table for P ∨ Q', () => {
+      const output = Calculator.generateTruthTable('P ∨ Q');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['P', 'Q', 'P ∨ Q']);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [false, true, true, true]);
+    });
+
+    it('should generate a truth table for (A ∨ B) -> ¬(¬C ∧ D)', () => {
+      const output = Calculator.generateTruthTable('(A ∨ B) -> ¬(¬C ∧ D)');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, [
+        'A',
+        'B',
+        'C',
+        'D',
+        '(A ∨ B) -> ¬(¬C ∧ D)',
+      ]);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 1, 0, 0],
+        [0, 1, 0, 1],
+        [0, 1, 1, 0],
+        [0, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1],
+        [1, 0, 1, 0],
+        [1, 0, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 0, 1],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [
+        true,
+        true,
+        true,
+        true,
+        true,
+        false,
+        true,
+        true,
+        true,
+        false,
+        true,
+        true,
+        true,
+        false,
+        true,
+        true,
+      ]);
+    });
+
+    it('should generate a truth table for ¬(A ∧ B)', () => {
+      const output = Calculator.generateTruthTable('¬(A ∧ B)');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['A', 'B', '¬(A ∧ B)']);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [true, true, true, false]);
+    });
+
+    it('should generate a truth table for (A -> B) ∧ (B -> A)', () => {
+      const output = Calculator.generateTruthTable('(A -> B) ∧ (B -> A)');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['A', 'B', '(A -> B) ∧ (B -> A)']);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [true, false, false, true]);
+    });
+
+    it('should generate a truth table for (A ∨ B) -> (B -> A)', () => {
+      const output = Calculator.generateTruthTable('(A ∨ B) -> (B -> A)');
+
+      const [formulas, truthCombinations, mainFormulaValues] = output;
+      assert.deepStrictEqual(formulas, ['A', 'B', '(A ∨ B) -> (B -> A)']);
+      assert.deepStrictEqual(truthCombinations, [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+      ]);
+      assert.deepStrictEqual(mainFormulaValues, [true, false, true, true]);
     });
   });
 });
---
+++ src/utils/isPropositionalVariable.ts
@@ -0,0 +1,13 @@
+import { Formula } from "src/types/formulas/formula";
+import { PropositionalVariable } from "src/types/operations/propositional-variable";
+
+const propositionalVariablesList: string[] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  ];
+  
+export function isPropositionalVariable(
+    formula: Formula | string
+  ): formula is PropositionalVariable {
+    return typeof formula === 'string' && propositionalVariablesList.includes(formula);
+  }
